93d69b3b689a6c20d4ff12def317a8b609774a2d
703864911@qq.com <703864911@qq.com>
2023-04-20 10:02:58 +0800

    第一次 提交

--- gradlemvc-master/src/main/java/org/zh/springmvc/controller/IndexController.java
+++ gradlemvc-master/src/main/java/org/zh/springmvc/controller/IndexController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+
+
     @RequestMapping("home")
     public String home() {
         return "home";
     }
+
+
+
 }
